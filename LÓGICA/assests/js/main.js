90bd14496daa11c39c88124b35825354d16d4d8f
Azt1 <inho.ilha@hotmail.com>
2022-06-18 18:30:08 -0300

    commit

--- LÓGICA/assests/js/main.js
+++ LÓGICA/assests/js/main.js
@@ -1,1 +1,18 @@
+//Capturar o evento de submit do formulário
+const form = document.querySelector('#formulário');
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault ();
+    console.log('evento previnido.');
+    setResultado('olá mundo');
+});
+
+
+function setResultado (msg) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = document.createElement ('p');
+    p.classList.add('paragrafo-resultado');
+    p.innerHTML = 'qualquer coisa';
+    resultado.appendChild(p);
+}
