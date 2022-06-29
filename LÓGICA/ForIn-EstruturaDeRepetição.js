f2add55759ca58b1d965cf827c3d824b500b2bc4
Azt1 <inho.ilha@hotmail.com>
2022-06-28 21:26:59 -0300

    For in

---
+++ LÓGICA/ForIn-EstruturaDeRepetição.js
@@ -0,0 +1,13 @@
+// for in lê os indece ou chaves do objeto
+// indice = array/vetor 
+// chaves é um objeto
+
+//                1      2       3
+
+const frutas = ['pera', 'Uva', 'maça'];
+
+
+for (let index in frutas) {
+    console.log(frutas[index]);
+}
+
