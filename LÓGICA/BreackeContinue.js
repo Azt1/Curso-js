8c854942fa2665183f9401c2ea561a1a623b4fbf
Azt1 <inho.ilha@hotmail.com>
2022-06-30 22:26:29 -0300

    Break e Continue

---
+++ LÓGICA/BreackeContinue.js
@@ -0,0 +1,23 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//
+let i = 0;
+do {
+    let numero = numeros[i]
+
+    if (numero === 2) {
+        console.log('Pulei o numero 2');
+        i++;
+        continue;
+    }
+    console.log(numero);
+
+    if (numero === 7) {
+        console.log('o numero 7 foi encrontrado , Saindo...')
+        i++;
+        break;
+    }
+    
+
+    i++;  
+  } while (i < numeros.length);
