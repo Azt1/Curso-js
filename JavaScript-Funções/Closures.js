a09b768b362edf7209eaed60394b4999e6422a6a
Azt1 <inho.ilha@hotmail.com>
2022-07-12 20:34:43 -0300

    Closures

---
+++ JavaScript-Funções/Closures.js
@@ -0,0 +1,13 @@
+// Closure é quando uma função "lembra"
+// seu escopo léxico, mesmo quando a funçao
+// é executada fora desse escopo léxico\
+function retornaFunçao(nome) {
+    return function() {
+        return nome;
+    };
+}
+
+const funçao = retornaFunçao('Luiz');
+const funçao2 = retornaFunçao('Joao');
+console.dir(funçao);
+console.dir(funçao2);
