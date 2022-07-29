5ddf896ed1d04016bbc67f23310533aae697ee3c
Azt1 <inho.ilha@hotmail.com>
2022-07-29 16:34:25 -0300

    Método Splice

---
+++ JavaScript-Arrays/MétodoSplice.js
@@ -0,0 +1,16 @@
+const nomes = ['Igor', 'alexandre', 'rosemary', 'nathalia'];
+
+nomes.splice(1, 3);
+console.log(nomes);
+
+
+// nomes.splice(indice, delete, elem1, elem2)
+// se eu quisesse colocar ate um numero maximo o comando é number.MAX_VALUE
+
+
+// SIMULA O POP
+// const removidos = nomes.splice(-1, 1);
+
+
+//  SIMULA O SHIFT
+// const removidos = nomes.splice(0, 1);
