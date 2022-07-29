6f301bd510ffe90878d897ee8d391694d225bc0e
Azt1 <inho.ilha@hotmail.com>
2022-07-28 23:46:13 -0300

    Revisando Arrays

---
+++ JavaScript-Arrays/RevisandoArraysBásico.js
@@ -0,0 +1,16 @@
+const nomes = ['igor', 'alexandre', 'rosemary', 'nathalia', 'douglas'];
+
+
+
+
+// const novo = nomes.slice(1, 3); // VAI FATIAR O ARRAY 
+
+
+// nomes.push('joao') // PUSH VAI ADICIONAR UM OBJETO NO ARRAY
+//  unshift VAI ADICIONAR UM OBJETO NO COMEÇO DO ARRAY
+
+// const removido = nomes.pop(); // vai remover um objeto do array e vai adicionar ele na variavel
+// POP REMOVE DO FINAL O SHIFT REMOVE O DO COMEÇO 
+
+
+//console.log(nomes.length) // PARA VER O TAMANHO DO ARRAY
