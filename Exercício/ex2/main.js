f476f02968d89cefba3552aebcd937a2e6243570
Azt1 <inho.ilha@hotmail.com>
2022-08-25 18:32:29 -0300

    ex2

---
+++ Exercício/ex2/main.js
@@ -0,0 +1,8 @@
+function PositivoOuNegativo() {
+    var positivo = document.getElementById('num1').value
+
+    if (positivo <= 5)
+        alert('o número ' + positivo + ' é positivo')
+        else if (positivo <= 10 )
+            alert ('o número ' + positivo + ' é negativo')
+}
